a7c32f0e7985dc1ab5f48f0f6aa4ffa2102a3b95
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2019-11-13 21:12:23 -0800

    PHPstan

--- src/interfaces/Parser.php
+++ src/interfaces/Parser.php
@@ -3,9 +3,12 @@
 namespace Hiraeth\Markdown;
 
 /**
- *
+ * The minimal interface for a markdown parser
  */
 interface Parser
 {
+	/**
+	 * Parse a markdown string into rendered HTML
+	 */
 	public function parse(string $markdown): string;
 }
